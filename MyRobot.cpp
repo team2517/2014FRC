1a85c616c7d9f7d5aec0cd137cbeba0ddbe2f4bd
gint271 <gint271@gmail.com>
2014-02-16 19:33:05 -0800

    Removed additonal prevTheta = curTheta.

--- MyRobot.cpp
+++ MyRobot.cpp
@@ -437,7 +437,6 @@ public:
 				for(i=0; i<4; i++)
 				{
 					wheel[i].prevTurnVel = wheel[i].turnVel;
-					wheel[i].prevTheta = wheel[i].curTheta;
 				}
 	//			if (stick.GetRawButton(7))
 	//			{
