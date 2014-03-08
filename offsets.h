c56c81401a440cb648000384f6f7d6f6fe25f213
gint271 <gint271@gmail.com>
2014-03-08 13:48:43 -0800

    Offsets.

--- offsets.h
+++ offsets.h
@@ -1,7 +1,7 @@
-float flOffset = 21; 
-float frOffset = 1.97;
-float blOffset = 3.75; 
-float brOffset = 1.12; 
+float flOffset = 4.200; 
+float frOffset = 1.941;
+float blOffset = 1.268; 
+float brOffset = 3.585; 
 
 //Original values
 //float flOffset = 4.68;
