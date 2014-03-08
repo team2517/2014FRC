b580e15d4b0e904bb8ca2871c8dc21baaefbd11b
tylerscheuble <tylerscheuble@gmail.com>
2014-03-08 09:57:35 -0800

    Recalibrated

--- offsets.h
+++ offsets.h
@@ -1,4 +1,10 @@
-float flOffset = 4.68;
+float flOffset = 4.60; 
 float frOffset = 1.97;
-float blOffset = 3.63;
-float brOffset = 1.33;
+float blOffset = 3.75; 
+float brOffset = 1.12; 
+
+//Original values
+//float flOffset = 4.68;
+//float frOffset = 1.97;
+//float blOffset = 3.63;
+//float brOffset = 1.33;
