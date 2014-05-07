22c6fa4573a1394dc6d8948443c2caeba3b71810
gint271 <gint271@gmail.com>
2014-05-06 18:50:43 -0700

    Changed offsets.

--- offsets.h
+++ offsets.h
@@ -3,7 +3,7 @@ The absolute encoders have a direction that they always call zero.
 We subtract these offsets from their return voltage to make the 
 Direction we want to be zero.
 */
-#define FLOFFSET			4.265
+#define FLOFFSET			2.73
 #define FROFFSET			3.595
 #define BLOFFSET			1.295
 #define BROFFSET			3.595
