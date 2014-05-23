18e4a9c35350e76326a612876dd902c1af52ce41
tylerscheuble <tylerscheuble@gmail.com>
2014-05-22 20:04:24 -0700

    Create util.cpp

---
+++ util.cpp
@@ -0,0 +1,27 @@
+float deadBand(float axisValue)
+{
+	if(axisValue < -.05 || axisValue > .05)
+	{
+		return axisValue;
+	}
+	else
+	{
+		return 0.0;
+	}
+}
+
+float* readOffsets()
+{
+    FILE* curFile = fopen(OFFSETFILE, "r");
+    float *rin = malloc(sizeof(float)*4);
+    fread(rin, sizeof(float), 4 ,curFile );
+    fclose(rin);
+    return rin;
+}
+
+void writeOffsets(float * offsets)
+{
+	FILE* curFile = fopen(OFFSETFILE, "r");
+	fwrite(offsets, sizeof(float), 4 ,curFile;
+	fclose(offsets);
+}
